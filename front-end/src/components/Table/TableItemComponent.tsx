81c495fe68bd6a70eaf9bbacccfb8f9fa11f6a70
richqqrd <spam@richardmiddendorf.de>
2023-12-13 16:01:57 +0100

    added like functionality and additional book details

--- front-end/src/components/Table/TableItemComponent.tsx
+++ front-end/src/components/Table/TableItemComponent.tsx
@@ -1,22 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Book} from '../../domain/bookInterface';
+import noImage from '../../no-image.png';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import unlike_icon from '../../icon-unlike.svg'
+import like_icon from '../../icon-like.svg'
+import {updateItem} from '../../domain/API';
 
-export const TableItemComponent = function({cover, title, isbn, author, publisher, price}: Book) {
-    const [like, setLike] = useState(0);
 
-    const handleImageClick = function(){
-        setLike(like + 1);
+
+export const TableItemComponent = function({title, subtitle, isbn, abstract, author, publisher, price, numPages, cover, likes, userId}: Book) {
+    const [like, setLike] = useState(false);
+    const [selected, setSelected] = useState(false);
+
+    const handleBookClick = function(){
+        setSelected(!selected);
+    }
+
+    const handleLikeClick = function(e: React.MouseEvent){
+        e.stopPropagation();
+        updateLike(!like)
+        setLike(!like);
+    }
+
+
+    const updateLike = function(like: boolean){
+        let newLikes = undefined;
+        if (like){
+            newLikes = (likes || 0) + 1;
+        } else {
+            newLikes = likes !== undefined ? likes - 1 : 0;
+        }
+
+        if (newLikes < 0) {
+            newLikes = 0;
+        }
+        const newBook: Book = {
+            title: title,
+            subtitle: subtitle,
+            isbn: isbn,
+            abstract: abstract,
+            author: author,
+            publisher: publisher,
+            price: price,
+            numPages: numPages,
+            cover: cover,
+            likes: newLikes,
+            userId: userId
+        };
+
+        updateItem(isbn, newBook);
     }
 
     const maxLength = 20;
 
     return (
+
         <div className="book-item">
-            <img className="book-cover" src={cover}  alt="No Image!" />
-            <p className="book-title">{title.length > maxLength ? title.substring(0, maxLength - 3) + "..." : title}</p> 
+            <div className={`book-item-content ${selected ? 'selected' : ''}`} onClick={handleBookClick}>
+                <div className="book-cover-container">
+                    <img className="book-cover" src={cover ? cover : noImage}/>
+                    <img className={`book-like-icon ${like ? 'liked' : ''}`} src={like ? like_icon : unlike_icon} alt="Like" onClick={handleLikeClick}/>                
+                </div>
+                <p className="book-author">{author && author.length > maxLength ? author.substring(0, maxLength - 3) + "..." : author}</p>
+                <p className="book-title ">{title.length > maxLength ? title.substring(0, maxLength - 3) + "..." : title}</p> 
+                <p className="book-price">{price}</p>
+            </div>
+            <div className={"book-item-button"}>
+                {selected && <button className="book-button"><FontAwesomeIcon icon={faShoppingCart}/> Add to cart</button>}
+            </div>
         </div>
     )
 };
-  
-//TODO klickbare like funktion mit persistenter speicherung
-//TODO condinatonl rendering 
