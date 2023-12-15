8ebd967d92d58aeea980447b4bc46003cc9d0528
richqqrd <spam@richardmiddendorf.de>
2023-12-15 14:23:25 +0100

    implemented new way of fetching items

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,28 +4,41 @@ import { AppHeaderComponent } from './components/AppHeader/AppHeaderComponent';
 import { TableHeaderComponent } from './components/Table/TableHeaderComponent';
 import { Book } from './domain/bookInterface'
 import { getAllItems as fetchItems } from './domain/API';
+import {useBooks } from './domain/hooks'
+import {fetchState} from './domain/FetchStateEnum'
 
 function App() {
-  const [books, setBooks] = useState<Book[]>([]);
+  //const [books, setBooks] = useState<Book[]>([]);
+  const { books, state, error, refresh } = useBooks();
 
   useEffect(() => {
-    fetchItems().then(fetchedBooks => {
-      setBooks(fetchedBooks);
-    });
+    refresh()
   }, []);
 
+  if (state === fetchState.loading){
+    return <div>LOADING ITEMS...</div>
+  }
 
+  else if (state === fetchState.error){
+    return <div>ERROR...</div>
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AppHeaderComponent />
-      </header>
-      <body className="App-table">
-        <TableHeaderComponent books={books}/>
-      </body>
-    </div>
-  );
+  else if (state === fetchState.success) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <AppHeaderComponent />
+        </header>
+        <body className="App-table">
+          <TableHeaderComponent books={books}/>
+        </body>
+      </div>
+    );
+  }
+
+  else {
+    return <div></div>
+  }
 }
 
 export default App;
