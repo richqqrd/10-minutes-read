c71919952d05a8b9b9da4fe0cae61253ceedf9fa
richqqrd <spam@richardmiddendorf.de>
2023-12-16 13:54:08 +0100

    updated App

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,44 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import { AppHeaderComponent } from './components/AppHeader/AppHeaderComponent';
+import { FooterComponent } from './components/Footer/FooterComponent';
 import { TableHeaderComponent } from './components/Table/TableHeaderComponent';
-import { Book } from './domain/bookInterface'
-import { getAllItems as fetchItems } from './domain/API';
-import {useBooks } from './domain/hooks'
-import {fetchState} from './domain/FetchStateEnum'
+import { Outlet } from "react-router-dom";
+
 
 function App() {
-  //const [books, setBooks] = useState<Book[]>([]);
-  const { books, state, error, refresh } = useBooks();
-
-  useEffect(() => {
-    refresh()
-  }, []);
-
-  if (state === fetchState.loading){
-    return <div>LOADING ITEMS...</div>
-  }
-
-  else if (state === fetchState.error){
-    return <div>ERROR...</div>
-  }
-
-  else if (state === fetchState.success) {
     return (
       <div className="App">
-        <header className="App-header">
           <AppHeaderComponent />
-        </header>
-        <body className="App-table">
-          <TableHeaderComponent books={books}/>
-        </body>
+          <Outlet />
+          <FooterComponent />
       </div>
     );
-  }
-
-  else {
-    return <div></div>
-  }
 }
 
 export default App;
