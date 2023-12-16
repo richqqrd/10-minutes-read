3d6a329b84816177900e70fd4764c966c0a2b024
richqqrd <spam@richardmiddendorf.de>
2023-12-16 13:53:05 +0100

    updated TableHeaderComponent

--- front-end/src/components/Table/TableHeaderComponent.tsx
+++ front-end/src/components/Table/TableHeaderComponent.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Book } from '../../domain/bookInterface';
 import { TableItemComponent } from './TableItemComponent';
+import {useBooks } from '../../domain/hooks'
+import {fetchState} from '../../domain/FetchStateEnum'
+import { LoadingComponent } from '../../components/Loading/LoadingComponent';
 
 
-export const TableHeaderComponent = function({books}: {books: Book[]}) {
-    return (
-        <div className="books-container">
-            {books.map(book => <TableItemComponent key={book.isbn} {...book} />)}
-        </div>
-    )
+
+
+
+export const TableHeaderComponent = function() {
+    const { books, state, error, refresh } = useBooks();
+
+    useEffect(() => {
+      refresh()
+    }, []);
+
+    if (state === fetchState.loading){
+        return <LoadingComponent />
+      }
+    
+    else if (state === fetchState.error){
+        return <div>ERROR...</div>
+      }
+    else {
+        return (
+            <div className="books-container">
+                {books.map(book => <TableItemComponent key={book.isbn} {...book} />)}
+            </div>
+        )
+    }
 }
