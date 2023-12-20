6ac6c011e0f103b4752ffea80986b61ed4456d8f
richqqrd <spam@richardmiddendorf.de>
2023-12-20 13:10:56 +0100

    Update AppHeaderComponent to include Add book link + TailwindCSS

--- front-end/src/components/AppHeader/AppHeaderComponent.tsx
+++ front-end/src/components/AppHeader/AppHeaderComponent.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../../10-minutes-read-logo.png'
+import { NavLink } from 'react-router-dom';
 
 export const AppHeaderComponent = function(){
     return ( 
-        <div className="App-header-content">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">10-minutes-read</h1>
+        <div className="flex justify-between items-center bg-gray-200 py-3 black font-bold">
+            <div className="flex items-center">
+                <img src={logo} className="h-10 mr-2 pl-4" alt="logo" />
+                <h1>10-minutes-read</h1>
+            </div>
+            <div className="pr-4">
+                <NavLink to="books/add" className="Add-link">Add book</NavLink>
+            </div>
         </div>
     )
 };
