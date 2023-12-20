cf724c6060afa907267658ff3a9b3c9ec5613c71
richqqrd <spam@richardmiddendorf.de>
2023-12-20 13:24:01 +0100

    Refactor FooterComponent to use tailwind CSS and add About link

--- front-end/src/components/Footer/FooterComponent.tsx
+++ front-end/src/components/Footer/FooterComponent.tsx
@@ -1,20 +1,22 @@
 import React  from 'react';
 import check from '../../check.svg';
+import { NavLink } from 'react-router-dom';
 
 
 
 
 export const FooterComponent = function() {
         return (
-            <div className="App-footer-container">
-                <div className='App-footer'>
-                    <img className="footer-check" src={check} alt="check"/>                
-                    <div className="footer-text">Kostenlose Retouren</div>
-                    <img className="footer-check" src={check} alt="check"/>                
-                    <div className="footer-text">Gratis-Buchversand innerhalb Deutschland</div>
-                    <img className="footer-check" src={check} alt="check"/>                
-                    <div className="footer-text">Click & Collect</div>
+            <div className="bg-gray-200 py-3">
+                <div className='flex items-center justify-center'>
+                    <img src={check} alt="check"/>                
+                    <div className="pr-3">Kostenlose Retouren</div>
+                    <img src={check} alt="check"/>                
+                    <div className="pr-3">Gratis-Buchversand innerhalb Deutschland</div>
+                    <img src={check} alt="check"/>                
+                    <div className="pr-3">Click & Collect</div>
                 </div>
+                <NavLink to="/about" className="font-bold text-lg">About</NavLink>
             </div>
         )
     }
