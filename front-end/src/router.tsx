313aa6b5f50eabf2d77d4aeda5ab081c9fcfbafa
richqqrd <spam@richardmiddendorf.de>
2023-12-20 09:32:59 +0100

    Add routes for item detail, edit, add, and delete components

--- front-end/src/router.tsx
+++ front-end/src/router.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import  App from './App';
 import { ErrorComponent }  from './components/Error/ErrorComponent'
 import { TableHeaderComponent } from "./components/Table/TableHeaderComponent";
+import { ItemDetailComponent } from "./components/Item/ItemDetailComponent";
+import {EditItemDetailComponent} from './components/Item/EditItemDetailComponent'
+import { AddItemComponent } from "./components/Table/AddItemComponent";
+import { DeleteItemDetailComponent } from './components/Item/DeleteItemDetailComponent'
 
 export const router = createBrowserRouter([
     {
@@ -11,7 +15,32 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: "", 
-                element: <TableHeaderComponent />
+                loader: () => redirect("books"),
+            },
+
+            {
+                path: "books",
+                element: <TableHeaderComponent />,
+            },
+
+            {
+                path: "books/:isbn",
+                element: <ItemDetailComponent />
+            },
+
+            { 
+                path: "books/:isbn/edit",
+                element: <EditItemDetailComponent />
+            },
+
+            {
+                path: "books/add",
+                element: <AddItemComponent />
+            },
+
+            {
+                path: "books/:isbn/delete",
+                element: <DeleteItemDetailComponent />
             }
         ]
     }
