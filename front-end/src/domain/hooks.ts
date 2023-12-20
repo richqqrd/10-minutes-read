ae73fd746c44a977152c070b3010e78d77c1cef5
richqqrd <spam@richardmiddendorf.de>
2023-12-20 09:29:02 +0100

    added additional custom Hooks

--- front-end/src/domain/hooks.ts
+++ front-end/src/domain/hooks.ts
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Book } from '../domain/bookInterface'
-import { getAllItems  } from '../domain/API';
+import { getAllItems,  getItemById, createItem,  updateItem, deleteItem  } from '../domain/API';
 import { fetchState } from './FetchStateEnum'
+
 
 
 
@@ -13,8 +14,6 @@ export const useBooks = () => {
 
 
         useEffect(() => {
-
-            console.log("update");
             setFetchState(fetchState.loading);
             getAllItems().then(fetchedBooks => {
                 setBooks(fetchedBooks);
@@ -38,4 +37,75 @@ export const useBooks = () => {
 
     return { books, state, error, refresh};
 };
+
+
+export const useBook = (isbn: string) => {
+    const [book, setBook] = useState<Book>();
+    const [state, setFetchState] = useState<fetchState>(fetchState.initial);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        setFetchState(fetchState.loading);
+    getItemById(isbn).then(fetchedBook => {
+        setBook(fetchedBook);
+        setFetchState(fetchState.success);
+    }).catch(error => {
+        setFetchState(fetchState.error);
+        setError(error);
+    });
+    }, [isbn]);
+
+    return {book, state, error }
+}
+
+export const useCreateBook = () => {
+    const [error, setError] = useState<Error | null>(null);
+
+    const createBook = (newBook: Book) => {
+        const response = createItem(newBook);
+        console.log(response);
+    }
+
+    return { createBook, error }
+}
+
+export const useUpdateBook = (isbn: string, updateBook: Book) => {
+    const [book, setBook] = useState<Book>();
+    const [state, setFetchState] = useState<fetchState>(fetchState.initial);
+    const [error, setError] = useState<Error | null>(null);
+
+    const update = () => {
+        setFetchState(fetchState.loading);
+        updateItem(isbn, updateBook).then(updatedBook => {
+            setBook(updateBook);
+            setFetchState(fetchState.success);
+        }).catch(error => {
+            setFetchState(fetchState.error);
+            setError(error);
+        });
+    };
+
+    return { book, state, error, update}
+}
+
+export const useDeleteBook = (isbn: string) => {
+    const [book, setBook] = useState<Book>();
+    const [state, setFetchState] = useState<fetchState>(fetchState.initial);
+    const [error, setError] = useState<Error | null>(null);
+
+    const deleteBook = () => {
+        setFetchState(fetchState.loading);
+        deleteItem(isbn).then(deletedBook => {
+            setBook(deletedBook);
+            setFetchState(fetchState.success);
+        }).catch(error => {
+            setFetchState(fetchState.error);
+            setError(error);
+        });
+    };
+
+    return { book, state, error, deleteBook}
+
+}
+
     
