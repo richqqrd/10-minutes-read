d8a002812b94d69c7216130235a90d728eb4e9b2
richqqrd <spam@richardmiddendorf.de>
2023-12-23 15:13:11 +0100

    Update AppHeaderComponent to include a Link to home page

--- front-end/src/components/AppHeader/AppHeaderComponent.tsx
+++ front-end/src/components/AppHeader/AppHeaderComponent.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from '../../10-minutes-read-logo.png'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export const AppHeaderComponent = function(){
     return ( 
         <div className="flex justify-between items-center bg-gray-200 py-3 black font-bold">
-            <div className="flex items-center">
+            <Link to="/books" className="flex items-center">
                 <img src={logo} className="h-10 mr-2 pl-4" alt="logo" />
                 <h1>10-minutes-read</h1>
-            </div>
+            </Link>
             <div className="pr-4">
-                <NavLink to="books/add" className="Add-link">Add book</NavLink>
+                <NavLink to="books/add">Add book</NavLink>
             </div>
         </div>
     )
