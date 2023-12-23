c226e6e65648d7f6342cd005a3f8b9d57693ba09
richqqrd <spam@richardmiddendorf.de>
2023-12-23 15:13:07 +0100

    Refactor ErrorComponent to display error message and handle route errors

--- front-end/src/components/Error/ErrorComponent.tsx
+++ front-end/src/components/Error/ErrorComponent.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
-export const ErrorComponent = function(){
+export const ErrorComponent: React.FC<{error: Error}> = ({error}) => {
+    const routeError = useRouteError();
+    const errorMessage = isRouteErrorResponse(routeError) ? routeError.statusText : routeError instanceof Error ? routeError.message : "An unknow error occurred";
     return ( 
-        <div>ERROR...</div>
+        <div>
+            <h1>Oops, something went wrong...</h1><div>{errorMessage}</div>
+            <h1>{error?.message}</h1>
+        </div>
     )
 };
