a0bafa0c8f40a57d893ecf388344971a6c751c58
richqqrd <spam@richardmiddendorf.de>
2023-12-23 15:13:25 +0100

    Remove unused CSS and table header component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
 import { AppHeaderComponent } from './components/AppHeader/AppHeaderComponent';
 import { FooterComponent } from './components/Footer/FooterComponent';
-import { TableHeaderComponent } from './components/Table/TableHeaderComponent';
 import { Outlet } from "react-router-dom";
 
 
 function App() {
     return (
-      <div className="App">
+      <div className='text-center min-h-screen'>
           <AppHeaderComponent />
           <Outlet />
           <FooterComponent />
