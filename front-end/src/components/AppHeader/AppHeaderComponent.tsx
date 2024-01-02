a1f63be244f5e3083db5b0f7cd75e5d3c62092c3
richqqrd <spam@richardmiddendorf.de>
2024-01-02 12:29:00 +0100

    Remove unnecessary Link component from AppHeaderComponent

--- front-end/src/components/AppHeader/AppHeaderComponent.tsx
+++ front-end/src/components/AppHeader/AppHeaderComponent.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../../10-minutes-read-logo.png'
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const AppHeaderComponent = function(){
     return ( 
         <div className="flex justify-between items-center bg-gray-200 py-3 black font-bold">
-            <Link to="/books" className="flex items-center">
+            <div className="flex items-center">
                 <img src={logo} className="h-10 mr-2 pl-4" alt="logo" />
                 <h1>10-minutes-read</h1>
-            </Link>
+            </div>
             <div className="pr-4">
+                <NavLink to="/books" className="mr-4">All books</NavLink>
                 <NavLink to="books/add">Add book</NavLink>
             </div>
         </div>
